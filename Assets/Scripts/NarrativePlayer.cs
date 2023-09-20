de4497c4b33f1506fa4b4f6f69a380905fd04f7c
Michał Biernat <44023124+m-biernat@users.noreply.github.com>
2023-09-20 23:48:39 +0200

    Add narrative player

---
+++ Assets/Scripts/NarrativePlayer.cs
@@ -0,0 +1,53 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using TMPro;
+
+[RequireComponent(typeof(Collider))]
+public class NarrativePlayer : UdonSharpBehaviour
+{
+    public GameObject container;
+
+    public AudioSource audioSource;
+
+    public TMP_Text textComponent;
+
+#if UNITY_EDITOR
+    [Space, TextArea(1, 25)]
+    public string text;
+
+    [Space]
+    public AudioClip clip;
+#endif
+
+    void Start()
+    {
+        StopAndHide();
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        base.OnPlayerTriggerEnter(player);
+
+        ShowAndPlay();
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        base.OnPlayerTriggerExit(player);
+
+        StopAndHide();
+    }
+
+    void ShowAndPlay()
+    {
+        container.SetActive(true);
+        audioSource.Play();
+    }
+
+    void StopAndHide()
+    {
+        audioSource.Stop();
+        container.SetActive(false);
+    }
+}
