f5053630168153ba4153243fe0cfff43901fba31
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2023-10-05 23:50:23 +0200

    Add dummy and animator switches

---
+++ Assets/Scripts/WaveOffKanagawa/AnimationOnOffSwitch.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AnimationOnOffSwitch : UdonSharpBehaviour
+{
+    [SerializeField] private Animator targetAnimator;
+    [SerializeField] private string boolName;
+    public bool isOn;
+
+    private void Start()
+    {
+        targetAnimator.SetBool(boolName, isOn);
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        isOn = !isOn;
+        targetAnimator.SetBool(boolName, isOn);
+    }
+}
