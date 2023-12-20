2e65e911501ec6d075bd7d80638a2d1b64067af1
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2023-12-20 18:13:55 +0100

    Reset RaySkybox variables on room load (#76)

---
+++ Assets/Scripts/RoomSpecific/ThePersistenceOfMemory/PersistenceOfMemoryLoader.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PersistenceOfMemoryLoader : Loadable
+{
+	[SerializeField] private RaySkyboxDatabase raySkyboxDatabase;
+	
+	public override void Load()
+	{
+		raySkyboxDatabase.Reset();
+	}
+
+	public override void Unload() {}
+}
