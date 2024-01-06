4ffcc9e8e5c47716cbf51e889a7d9ddfa6f5dfc4
MagicalMatrix <magical0matrix@gmail.com>
2024-01-07 00:10:22 +0100

    PersistanceOfMemory

--- Assets/Scripts/RoomSpecific/ThePersistenceOfMemory/RaySkyboxDatabase.cs
+++ Assets/Scripts/RoomSpecific/ThePersistenceOfMemory/RaySkyboxDatabase.cs
@@ -5,6 +5,9 @@ using VRC.Udon;
 
 public class RaySkyboxDatabase : ShaderPasser
 {
+	[SerializeField] private SkinnedMeshRenderer treeMesh;
+	[SerializeField] private ClockAnimator[] clockAnimators;
+	[SerializeField] private ClockCentralizer[] clockCentralizers;
 	[SerializeField] private Transform[] markers;
 	[SerializeField] private int currentMarker;
 	[SerializeField] private float activationDistance = 0.05f;
@@ -65,5 +68,27 @@ public class RaySkyboxDatabase : ShaderPasser
 		lerp = lerp < 0 ? 0 : lerp;
 		lerp = lerpCurve.Evaluate(lerp);
 		MainMaterial.SetVector(PropertyIDs[currentMarker], new Vector4(lerp, YScale, lerp, lerp));
+
+		for (int i = 0; i < clockAnimators.Length; i++)
+		{
+			clockAnimators[i].reanimate(lerp);
+		}
+
+		float markerPart = 1f / (float)(markers.Length - 1);
+		for (int i = 0; i < clockCentralizers.Length; i++)
+		{
+			clockCentralizers[i].threshold = markerPart * currentMarker + lerp * markerPart;
+		}
+
+		if (currentMarker % 2 > 0)
+		{
+			treeMesh.SetBlendShapeWeight(0, 100 - (markerPart * currentMarker + lerp * markerPart * 400));
+		}
+		else
+		{
+			treeMesh.SetBlendShapeWeight(0, markerPart * currentMarker + lerp * markerPart * 400);
+		}
+
+		Debug.Log(markerPart * currentMarker + lerp * markerPart * 100);
 	}
 }
